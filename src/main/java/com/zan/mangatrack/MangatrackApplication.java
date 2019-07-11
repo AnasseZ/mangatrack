e743f1e3832ba4d1f754f7a6441c3d15b18fa57a
Anasse Zougarh <zougarh.anasse@gmail.com>
2019-07-11 22:24:21 +0200

    lets rewrite this

---
+++ src/main/java/com/zan/mangatrack/MangatrackApplication.java
@@ -0,0 +1,13 @@
+package com.zan.mangatrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangatrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangatrackApplication.class, args);
+	}
+
+}
