4f1564c70ef45001adaad9fb343d96b6c5b9f661
Anasse Zougarh <zougarh.anasse@gmail.com>
2019-10-18 22:21:12 +0200

    rewrote authcontext in react 16

--- src/main/react/src/contexts/AuthContext.js
+++ src/main/react/src/contexts/AuthContext.js
@@ -1,132 +1,100 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-import { apiRoot, tokenName } from "../constantes/apiInformations";
-import { getCurrentUser, getUserToken } from "../services/UserService";
+import {tokenName} from "../constantes/apiInformations";
+import {getCurrentUser, getUserToken} from "../services/UserService";
 import LoadingPage from "../components/pages/loading/LoadingPage";
 import {addErrorNotification, addNotification, addSuccessNotification} from "../util/notification";
 
 const AuthContext = React.createContext();
 
-class AuthProvider extends React.Component {
-  constructor() {
-    super();
-    this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
-    this.verifyUser = this.verifyUser.bind(this);
-    this.userConnexionOk = this.userConnexionOk.bind(this);
-    this.userConnexionError = this.userConnexionError.bind(this);
-    this.updateIsAuth = this.updateIsAuth.bind(this);
-    this.updateIsLoaded = this.updateIsLoaded.bind(this);
-    this.updateToken = this.updateToken.bind(this);
-    this.setToken = this.setToken.bind(this);
-
-    this.state = {
-      isLoaded: false,
-      isAuth: false,
-      apiRoot: apiRoot,
-      token: "",
-      user: null,
-      error: null
+const AuthProvider = (props) => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [isAuth, setIsAuth] = useState(false);
+    const [token, setToken] = useState("");
+    const [user, setUser] = useState(null);
+    const [error, setError] = useState(null);
+    
+    const login = (userId, password) => {
+        getUserToken(
+            userId,
+            password,
+            token => {
+                localStorage.setItem(tokenName, token.accessToken);
+                verifyUser();
+                addSuccessNotification("Connexion réussie.");
+            },
+            userConnexionError
+        );
     };
-  }
 
-  login(userId, password) {
-    getUserToken(
-      userId,
-      password,
-      token => {
-        this.setToken(token);
-        this.verifyUser();
-        addSuccessNotification("Connexion réussie.");
-      },
-      this.userConnexionError
-    );
-  }
+    useEffect(() => {
+        verifyUser();
+    }, []);
 
-  componentDidMount() {
-    this.verifyUser();
-  }
+    const logout = () => {
+        updateIsAuth(false);
+        localStorage.removeItem(tokenName);
+        addNotification("Déconnexion réussie.", 'info');
+    };
 
-  logout() {
-    this.updateIsAuth(false);
-    localStorage.removeItem(tokenName);
-    addNotification("Déconnexion réussie.", 'info');
-  }
+    const userConnexionOk = user => {
+        setUser(user);
+        updateIsAuth(true);
+        setIsLoaded(true);
 
-  userConnexionOk(user) {
-    this.setState({ user: user });
-    this.updateIsAuth(true);
-    this.updateIsLoaded(true);
+        // pas d'erreur token OK
+        setToken(localStorage.getItem(tokenName));
+    };
 
-    // pas d'erreur token OK et userId aussi
-    this.updateToken();
-  }
+    const userConnexionError = error => {
+        setError(error);
+        setIsLoaded(true);
 
-  updateIsLoaded(isLoaded) {
-    this.setState({ isLoaded: isLoaded });
-  }
+        // Erreur donc token réinitialisé
+        localStorage.removeItem(tokenName);
+        setToken(localStorage.getItem(tokenName));
 
-  updateToken() {
-    this.setState({ token: localStorage.getItem(tokenName) });
-  }
+        addErrorNotification("Erreur ! Vous n'avez pas pu vous connectez.");
+    };
 
-  setToken(token) {
-    localStorage.setItem(tokenName, token.accessToken);
-  }
+    const updateIsAuth = isAuth => {
+        setIsAuth(isAuth);
+    };
 
-  userConnexionError(error) {
-    this.setState({ error: error });
-    this.updateIsLoaded(true);
+    const verifyUser = () => {
+        let tokenStored = localStorage.getItem(tokenName);
 
-    // Erreur donc token réinitialisé et userId aussi
-    localStorage.removeItem(tokenName);
+        // Si le token existe ( user deja co sur la machine... on verifie)
+        if (tokenStored !== null) {
+            // envoyer au serveur le token pour recuperer son profil
+            getCurrentUser(
+                tokenStored,
+                userConnexionOk,
+                userConnexionError
+            );
+        } else {
+            // Premiere visite, on load la page principale
+            setIsLoaded(true);
+        }
+    };
 
-    this.updateToken()
-    addErrorNotification("Erreur ! Vous n'avez pas pu vous connectez.");
-  }
-
-  updateIsAuth(isAuth) {
-    this.setState({ isAuth: isAuth });
-  }
-
-  verifyUser() {
-    let tokenStored = localStorage.getItem(tokenName);
-
-    // Si le token et userId existent ( user deja co... on verifie)
-    if (tokenStored !== null) {
-      // envoyer au serveur le token pour recuperer son profil
-      getCurrentUser(
-        tokenStored,
-        this.userConnexionOk,
-        this.userConnexionError
-      );
-    } else {
-      // Premiere visite, on load la page principale
-      this.updateIsLoaded(true);
-    }
-  }
-
-  render() {
-    const { isLoaded } = this.state;
     return isLoaded === true ? (
-      <AuthContext.Provider
-        value={{
-          isAuth: this.state.isAuth,
-          apiRoot: this.state.apiRoot,
-          login: this.login,
-          logout: this.logout,
-          user: this.state.user,
-          token: this.state.token
-        }}
-      >
-        {this.props.children}
-      </AuthContext.Provider>
+        <AuthContext.Provider
+            value={{
+                isAuth: isAuth,
+                login: login,
+                logout: logout,
+                user: user,
+                token: token
+            }}
+        >
+            {props.children}
+        </AuthContext.Provider>
     ) : (
-      <LoadingPage />
+        <LoadingPage/>
     );
-  }
-}
+};
 
 const AuthConsumer = AuthContext.Consumer;
 
-export { AuthProvider, AuthConsumer };
+export {AuthProvider, AuthConsumer};
